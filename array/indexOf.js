3bfb5db25c45c795103e38ad543f1f09ee9d47c2
Rabson <yogeshNishad664@gmail.com>
2019-12-20 21:24:10 +0530

    added new code

---
+++ array/indexOf.js
@@ -0,0 +1,9 @@
+const names = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+for (let el of arrMap) {
+    arr.lastIndexOf(el);
+}
+
+for (let el of arrMap) {    
+    arr.indexOf(el);
+}
---
+++ array/maps.js
@@ -0,0 +1,5 @@
+const arr = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+const arrMap = new Set(arr);
+
+console.log([...arrMap]);
